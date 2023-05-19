e76d40e58557a382de6de1bc248e248a36e04df6
javierbarrera574 <82546878+javierbarrera574@users.noreply.github.com>
2023-05-18 21:14:11 -0300

    Add files via upload

---
+++ EjercicioRuby.rb
@@ -0,0 +1,18 @@
+
+#Inventar un ejercicio en donde se apliquen al menos:
+#Dos tipos de datos.
+#Dos estructuras de control (Queda a elección)
+
+nombre = 'Franco'
+edad = 30
+
+contador = 1
+while contador <=1
+  if edad >= 18
+  puts "#{nombre} es mayor de edad."
+else
+  puts "#{nombre} es menor de edad."
+end
+  puts
+    contador += 1
+end
